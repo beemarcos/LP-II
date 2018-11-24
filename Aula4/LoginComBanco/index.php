dc67dcd43f48a1feb3191fd878cd7d7b2adcebb4
Marcos Bee <beemarcos72@gmail.com>
2018-11-24 12:13:59 -0200

    Iniciando o projeto

---
+++ Aula4/LoginComBanco/index.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once 'bootload.php';
+//Mensagem flash de erro
+if ( isset($_SESSION['error']) ) {
+    $msg = new \Plasticbrain\FlashMessages\FlashMessages();
+    $msg->error('Usuário ou senha incorretos');
+    $msg->display();
+    unset($_SESSION['error']);
+}
+
+?>
+
+<html>
+    <head>
+    <link href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet"/>
+    <script src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    </head>
+<body id="LoginForm">
+    <div class="container">
+    <div class="login-form">
+    <div class="main-div">
+        <div class="panel">
+    <h2>Login do usuário</h2>
+    <?php    
+        if( isset($_GET['erro']) ){
+            echo '<p style="color: red">Usuário ou senha incorretos!</p>';
+        }else{
+            echo '<p>Por favor preencha com seu usuário e senha:</p>';
+        }
+    ?>
+    </div>
+        <form id="Login" action="verifica.php" method="post">
+            <div class="form-group">
+                <input type="email" name="email" class="form-control" id="inputEmail" placeholder="Endereço de email">
+            </div>
+            <div class="form-group">
+                <input type="password" name="senha" class="form-control" id="inputPassword" placeholder="Senha">
+            </div>
+            <button type="submit" class="btn btn-primary">Entrar</button>
+        </form>
+        </div>
+    </div>
+    </div>
+    </div>
+</body>
+</html>
