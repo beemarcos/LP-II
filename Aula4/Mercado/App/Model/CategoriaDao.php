84cacb74a5c05ae0695a956269484f785eb991dd
Marcos Bee <beemarcos72@gmail.com>
2018-11-30 10:19:26 -0200

    Projeto antes do composer
    composer dump-autoload

--- Aula4/Mercado/App/Model/CategoriaDao.php
+++ Aula4/Mercado/App/Model/CategoriaDao.php
@@ -7,17 +7,47 @@ class CategoriaDao
     public function create ( Categoria $c )
     {
         //cadastra uma categoria
+        $stmt = Conexao::getConexao()->prepare( 'INSERT INTO categorias (nome) VALUES (:nome)');
+        $stmt->bindParam(':nome', $c->getNome());
+        if ( $stmt->execute() ){
+            return true;
+        } else {
+            return false;
+        }
     }
     public function read ()
     {
         //le as categorias
+        $sql = 'SELECT * FROM categorias';
+        $enviar = Conexao::getConexao()->prepare($sql);
+        $enviar->execute();
+        if ( $enviar->rowCount() >0 ) {
+            $resultado = $enviar->fetchAll(PDO::FETCH_ASSOC);
+            return $resultado;
+        }
+        return [];
     }
     public function update ( Categoria $c )
     {
         //atualiza os dados da categoria
+        $stmt = Conexao::getConexao()->prepare( 'UPDATE categorias SET nome = :nome WHERE id = :id');
+        $stmt->bindParam(':nome', $c->getNome());
+        $stmt->bindParam(':id', $c->getId());
+        if ( $stmt->execute() ){
+            return true;
+        } else {
+            return false;
+        }
     }
     public function delete ( $codigo )
     {
         //remove a categoria
+        $stmt = Conexao::getConexao()->prepare( 'DELETE FROM categorias WHERE id = :id');
+        $stmt->bindParam(':id', $codigo);
+        if ( $stmt->execute() ){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
--- Aula4/Mercado/App/Model/ProdutoDao.php
+++ Aula4/Mercado/App/Model/ProdutoDao.php
@@ -7,17 +7,49 @@ class ProdutoDao
     public function create ( Produto $p )
     {
         //cadastra um produto
+        $stmt = Conexao::getConexao()->prepare( 'INSERT INTO produtos (nome, id_categoria) VALUES (:nome,:id_categoria)');
+        $stmt->bindParam(':nome', $p->getNome());
+        $stmt->bindParam(':id_categoria', $p->getId_categoria());
+        if ( $stmt->execute() ){
+            return true;
+        } else {
+            return false;
+        } 
     }
     public function read ()
     {
         //le os produtos
+        $sql = 'SELECT * FROM produtos';
+        $enviar = Conexao::getConexao()->prepare($sql);
+        $enviar->execute();
+        if ( $enviar->rowCount() >0 ) {
+            $resultado = $enviar->fetchAll(PDO::FETCH_ASSOC);
+            return $resultado;
+        }
+        return [];
     }
     public function update ( Produto $p )
     {
         //atualiza os dados do produto
+        $stmt = Conexao::getConexao()->prepare( 'UPDATE produtos SET nome = :nome, id_categoria = :id_categoria WHERE id = :id');
+        $stmt->bindParam(':nome', $p->getNome());
+        $stmt->bindParam(':id', $p->getId());
+        $stmt->bindParam(':id_categoria', $p->getId_categoria());
+        if ( $stmt->execute() ){
+            return true;
+        } else {
+            return false;
+        }
     }
     public function delete ( $codigo )
     {
         //remove o produto
+        $stmt = Conexao::getConexao()->prepare( 'DELETE FROM produtos WHERE id = :id');
+        $stmt->bindParam(':id', $codigo);
+        if ( $stmt->execute() ){
+            return true;
+        } else {
+            return false;
+        }
     }
 }
